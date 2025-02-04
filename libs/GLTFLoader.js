22c6899cada85f3db4a158ea26ea1a3120f4be4f
solraczo <79017916+solraczo@users.noreply.github.com>
2025-02-04 09:13:03 -0500

    Update GLTFLoader.js

--- libs/GLTFLoader.js
+++ libs/GLTFLoader.js
@@ -64,7 +64,7 @@ import {
 	VectorKeyframeTrack,
 	SRGBColorSpace,
 	InstancedBufferAttribute
-} from 'https://miichipost.com/viewport/3DModelWebAR2/libs/three.module.js';
+} from 'https://github.com/solraczo/Sistema_Solar_AR_1/tree/main/libs/three.module.js';
 
 import { toTrianglesDrawMode } from '../libs/BufferGeometryUtils.js';
 
