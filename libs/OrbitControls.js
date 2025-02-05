34d9865dee8faf170921b048123b7c88184c1244
solraczo <79017916+solraczo@users.noreply.github.com>
2025-02-04 20:59:46 -0500

    Update OrbitControls.js

--- libs/OrbitControls.js
+++ libs/OrbitControls.js
@@ -9,7 +9,7 @@ import {
 	Plane,
 	Ray,
 	MathUtils
-} from 'https://github.com/solraczo/Sistema_Solar_AR_1/tree/main/libs/three.module.js';
+} from 'https://github.com/solraczo/Sistema_Solar_AR_1/libs/three.module.js';
 
 // OrbitControls performs orbiting, dollying (zooming), and panning.
 // Unlike TrackballControls, it maintains the "up" direction object.up (+Y by default).
